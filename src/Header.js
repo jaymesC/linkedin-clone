cbea4a614ba371ae561ec49aed9c0748cd329c4f
jaymesC <clementjaymes@gmail.com>
2021-11-17 07:57:13 -0800

    updated Header component

--- src/Header.js
+++ src/Header.js
@@ -1,26 +1,24 @@
-import React from 'react'
-import './Header.css'
-import SearchIcon from '@material-ui/icons/Search'
+import React from "react";
+import "./Header.css";
+import SearchIcon from "@material-ui/icons/Search";
 
 function Header() {
-    return (
-      <div className="header">
-        <h1>Header</h1>
-
-        <div className="header__left">
-          <img
-            src="https://cdn-icons.flaticon.com/png/512/3536/premium/3536505.png?token=exp=1637132651~hmac=2800003123864f171366d33e0e842f03"
-            alt=""
-          />
-          <div className="header__search">
-            <SearchIcon />
-            <input type="text" />
-          </div>
+  return (
+    <div className="header">
+      <div className="header__left">
+        <img
+          src="https://cdn-icons.flaticon.com/png/512/3536/premium/3536505.png?token=exp=1637132651~hmac=2800003123864f171366d33e0e842f03"
+          alt=""
+        />
+        <div className="header__search">
+          <SearchIcon />
+          <input type="text" />
         </div>
-
-        <div className="header__right"></div>
       </div>
-    );
+
+      <div className="header__right"></div>
+    </div>
+  );
 }
 
-export default Header
+export default Header;
