fb5e53dffdcdd821389f0a648a978f323ed13de8
jaymesC <clementjaymes@gmail.com>
2021-11-17 08:04:10 -0800

    created and updated HeaderOption component

---
+++ src/HeaderOption.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './HeaderOption.css'
+
+function HeaderOption() {
+    return (
+        <div className="header__option">
+            
+        </div>
+    )
+}
+
+export default HeaderOption
