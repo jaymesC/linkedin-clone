1295deeb9169d86200862b03d22bbf8860e2ed95
jaymesC <clementjaymes@gmail.com>
2021-11-17 21:36:19 -0800

    updated Header component

--- src/Header.js
+++ src/Header.js
@@ -1,6 +1,9 @@
 import React from "react";
 import "./Header.css";
 import SearchIcon from "@material-ui/icons/Search";
+import HomeIcon from "@material-ui/icons/Home";
+import SupervisorAccountIcon from "@material-ui/icons/SupervisorAccount";
+import HeaderOption from "./HeaderOption";
 
 function Header() {
   return (
@@ -16,7 +19,10 @@ function Header() {
         </div>
       </div>
 
-      <div className="header__right"></div>
+      <div className="header__right">
+          <HeaderOption Icon={HomeIcon} title="Home" />
+          <HeaderOption Icon={SupervisorAccountIcon} title="My Network" />
+      </div>
     </div>
   );
 }
