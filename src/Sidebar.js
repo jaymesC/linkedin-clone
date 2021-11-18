da715b9eb484473f3faa2e52b15b9143e7a8633b
jaymesC <clementjaymes@gmail.com>
2021-11-18 09:54:57 -0800

    updated Header component

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,8 +1,16 @@
+import { Avatar } from '@material-ui/core'
 import React from 'react'
+import './Sidebar.css'
 
 function Sidebar() {
     return (
-        <div>
+        <div className="sidebar">
+            <div className="sidebar__top">
+                <img src="" alt="" />
+                <Avatar className="sidebar__avatar"/>
+                <h2>Clement James</h2>
+                <h4>clementjaymes@gmail.com</h4>
+            </div>
             
         </div>
     )
