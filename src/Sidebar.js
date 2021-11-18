2b0a531daad45a80b2c9b61af4c0a7d5007d33ac
jaymesC <clementjaymes@gmail.com>
2021-11-18 10:00:45 -0800

    updated Sidebar component

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -4,16 +4,30 @@ import './Sidebar.css'
 
 function Sidebar() {
     return (
-        <div className="sidebar">
-            <div className="sidebar__top">
-                <img src="" alt="" />
-                <Avatar className="sidebar__avatar"/>
-                <h2>Clement James</h2>
-                <h4>clementjaymes@gmail.com</h4>
-            </div>
-            
+      <div className="sidebar">
+        <div className="sidebar__top">
+          <img src="" alt="" />
+          <Avatar className="sidebar__avatar" />
+          <h2>Clement James</h2>
+          <h4>clementjaymes@gmail.com</h4>
         </div>
-    )
+
+        <div className="sidebar__stats">
+          <div className="sidebar__stat">
+            <p>Who viewed you</p>
+            <p className="sidebar__statNumber">2,671</p>
+          </div>
+          <div className="sidebar__stat">
+            <p>Who viewed you</p>
+            <p className="sidebar__statNumber">2,506</p>
+          </div>
+        </div>
+
+        <div className="sidebar__bottom">
+            <p>Recent</p>
+        </div>
+      </div>
+    );
 }
 
 export default Sidebar
