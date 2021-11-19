b4062597fcb6598c0ba84f4073c38dbd5de878b3
jaymesC <clementjaymes@gmail.com>
2021-11-19 02:31:43 -0800

    updated Sidebar component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import './App.css';
-import Header from './Header';
-import Sidebar from './Sidebar';
+import React from "react";
+import "./App.css";
+import Header from "./Header";
+import Sidebar from "./Sidebar";
 
 function App() {
   return (
@@ -12,8 +12,6 @@ function App() {
         <Sidebar />
 
       </div>
-
-    
     </div>
   );
 }
--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,33 +1,36 @@
-import { Avatar } from '@material-ui/core'
-import React from 'react'
-import './Sidebar.css'
+import { Avatar } from "@material-ui/core";
+import React from "react";
+import "./Sidebar.css";
 
 function Sidebar() {
-    return (
-      <div className="sidebar">
-        <div className="sidebar__top">
-          <img src="" alt="" />
-          <Avatar className="sidebar__avatar" />
-          <h2>Clement James</h2>
-          <h4>clementjaymes@gmail.com</h4>
-        </div>
+  return (
+    <div className="sidebar">
+      <div className="sidebar__top">
+        <img
+          src="https://images.unsplash.com/photo-1579546929518-9e396f3cc809?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80"
+          alt=""
+        />
+        <Avatar className="sidebar__avatar" />
+        <h2>Clement James</h2>
+        <h4>clementjaymes@gmail.com</h4>
+      </div>
 
-        <div className="sidebar__stats">
-          <div className="sidebar__stat">
-            <p>Who viewed you</p>
-            <p className="sidebar__statNumber">2,671</p>
-          </div>
-          <div className="sidebar__stat">
-            <p>Who viewed you</p>
-            <p className="sidebar__statNumber">2,506</p>
-          </div>
+      <div className="sidebar__stats">
+        <div className="sidebar__stat">
+          <p>Who viewed you</p>
+          <p className="sidebar__statNumber">2,671</p>
         </div>
-
-        <div className="sidebar__bottom">
-            <p>Recent</p>
+        <div className="sidebar__stat">
+          <p>Who viewed you</p>
+          <p className="sidebar__statNumber">2,506</p>
         </div>
       </div>
-    );
+
+      <div className="sidebar__bottom">
+        <p>Recent</p>
+      </div>
+    </div>
+  );
 }
 
-export default Sidebar
+export default Sidebar;
