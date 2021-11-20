1e8e19ba7e553765ab02683bc16dc5f73be55cf6
jaymesC <clementjaymes@gmail.com>
2021-11-20 04:18:47 -0800

    updated Post component

--- src/Post.js
+++ src/Post.js
@@ -1,4 +1,9 @@
 import { Avatar } from '@material-ui/core';
+import InputOption from './InputOption';
+import ThumbUpAltOutlinedIcon from '@material-ui/icons/ThumbUpAltOutlined'
+import ChatOutlinedIcon from '@material-ui/icons/ChatOutlined';
+import ShareOutlinedIcon from '@material-ui/icons/ShareOutlined';
+import SendOutlinedIcon from '@material-ui/icons/SendOutlined';
 import './Post.css';
 
 function Post({name, description, message, photoUrl}) {
@@ -7,12 +12,19 @@ function Post({name, description, message, photoUrl}) {
             <div className="post__header">
                 <Avatar />
                 <div className="post__info">
-                    <h2>Jay</h2>
-                    <p>Description</p>
+                    <h2>{name}</h2>
+                    <p>{description}</p>
                 </div>
             </div>
             <div className="post__body">
-                <p>Message</p>
+                <p>{message}</p>
+            </div>
+
+            <div className="post__buttons">
+                <InputOption Icon={ThumbUpAltOutlinedIcon} title="Like" color="gray"/>
+                <InputOption Icon={ChatOutlinedIcon} title="Comment" color="gray"/>
+                <InputOption Icon={ShareOutlinedIcon} title="Share" color="gray"/>
+                <InputOption Icon={SendOutlinedIcon} title="Send" color="gray"/>
             </div>
         </div>
     )
