5685594c4ca7b6932e1bbbbb1ec12b7315be32b9
jaymesC <clementjaymes@gmail.com>
2021-11-24 08:56:31 -0800

    setting up redux

--- src/features/userSlice.js
+++ src/features/userSlice.js
@@ -9,27 +9,27 @@ import {  createSlice } from '@reduxjs/toolkit';
 
 
 export const userSlice = createSlice({
-  name: 'user',
+  name: "user",
   initialState: {
     user: null,
   },
   // The `reducers` field lets us define reducers and generate associated actions
   reducers: {
-    login: (state) => {
+    login: (state, action) => {
       // Redux Toolkit allows us to write "mutating" logic in reducers. It
       // doesn't actually mutate the state because it uses the Immer library,
       // which detects changes to a "draft state" and produces a brand new
       // immutable state based off those changes
-      state.value += 1;
+      state.user = action.payload;
     },
     // Use the PayloadAction type to declare the contents of `action.payload`
-    logout: (state, action) => {
-      state.value += action.payload;
+    logout: (state) => {
+      state.user = null;
     },
   },
 });
 
-export const { increment, decrement, incrementByAmount } = userSlice.actions;
+export const { login, logout } = userSlice.actions;
 
 // The function below is called a selector and allows us to select a value from
 // the state. Selectors can also be defined inline where they're used instead of
