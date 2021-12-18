63fcb154d85dee89f89bada9218fdcdfb47710ca
jaymesC <clementjaymes@gmail.com>
2021-12-18 15:31:46 -0800

    updated login component

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,8 +1,11 @@
 import { Avatar } from "@material-ui/core";
 import React from "react";
+import { useSelector } from "react-redux";
+import { selectUser } from "./features/userSlice";
 import "./Sidebar.css";
 
 function Sidebar() {
+  const user = useSelector(selectUser);
 
     const recentItem = (topic) => (
         <div className="sidebar__recentItem">
@@ -18,9 +21,9 @@ function Sidebar() {
           src="https://images.unsplash.com/photo-1579546929518-9e396f3cc809?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80"
           alt=""
         />
-        <Avatar className="sidebar__avatar" />
-        <h2>Clement James</h2>
-        <h4>clementjaymes@gmail.com</h4>
+        <Avatar src={user.photoUrl} className="sidebar__avatar">{user.displayName[0]}</Avatar>
+        <h2>{user.displayName}</h2>
+        <h4>{user.email}</h4>
       </div>
 
       <div className="sidebar__stats">
