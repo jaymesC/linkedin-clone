da1390f45de4677eef99008d7b47f74a96390e90
jaymesC <clementjaymes@gmail.com>
2021-12-31 16:33:19 -0800

    updated Widget Component

--- src/Widgets.js
+++ src/Widgets.js
@@ -1,8 +1,22 @@
 import React from "react";
 import "./Widget.css";
 import InfoIcon from "@material-ui/icons/Info";
+import FiberManualRecordIcon from "@material-ui/icons/FiberManualRecord";
 
 function Widgets() {
+
+    const newsArticle = (heading, subtitle) => {
+        <div className="widget__article">
+            <div className="widget__articleLeft">
+                <FiberManualRecordIcon />
+            </div>
+            <div className="widget__articleRight">
+                <h4>{heading }</h4>
+                <p>{subtitle }</p>
+            </div>
+        </div>
+    }
+
   return (
     <div className="widgets">
       <div className="widget__header">
